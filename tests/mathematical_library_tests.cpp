de092b3dce0c31a6260afe12f0578dace1141924
xsvehl09 <xsvehl09@stud.fit.vutbr.cz>
2018-03-10 23:38:11 +0100

    Test Dividing, Modulo, SquareRoot
    TDD tests for Dividing(), Modulo() and SquareRoot() functions.
    
    Signed-off-by: xsvehl09 <xsvehl09@stud.fit.vutbr.cz>

--- tests/mathematical_library_tests.cpp
+++ tests/mathematical_library_tests.cpp
@@ -7,9 +7,25 @@
 #include "googletest-master/googlemock/include/gmock/gmock.h"
 #include "../math_lib/mathematical_library.h"
 
-TEST(testiing, Addition)
+TEST(testing, Dividing)
 {
-	ASSERT_EQ(scitanie(5,5),10);
+  EXPECT_EQ(Divide(0, 42), 0);
+  ASSERT_EQ(Divide(196, 14), 14);
+  ASSERT_ANY_THROW(Divide(142, 0));
+}
+
+TEST(testing, Modulo)
+{
+  ASSERT_EQ(Modulo(0, 42), 0);
+  ASSERT_EQ(Modulo(196, 42), 28);
+  ASSERT_ANY_THROW(Modulo(142, 0));
+}
+
+TEST(testing, SquareRoot)
+{
+  ASSERT_EQ(SquareRoot(0), 0);
+  ASSERT_EQ(SquareRoot(196), 14);
+  ASSERT_ANY_THROW(SquareRoot(-42));
 }
 
 int main(int argc, char *argv[])
