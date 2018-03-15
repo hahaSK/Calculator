87ae81eb6d6aa260531cc120a5b4de130adb1ad3
xsvehl09 <xsvehl09@stud.fit.vutbr.cz>
2018-03-15 14:10:51 +0100

    Correction of mathematical library header file
    Name of input parameters edited from param1 param2 to more accurate names "leftOperand" and "rightOperand".
    Fixed the modulo function declaration - modulo is supposed to work only with integer numbers.
    Added comments to Modulo and Multiply functions.
    
    Signed-off-by: xsvehl09 <xsvehl09@stud.fit.vutbr.cz>

--- src/math_lib/mathematical_library.cpp
+++ src/math_lib/mathematical_library.cpp
@@ -11,25 +11,25 @@
 
 #include "mathematical_library.h"
 
-double Subtract(double param1, double param2)
+double Subtract(double leftOperand, double rightOperand)
 {
-  return param1 - param2;
+  return leftOperand - rightOperand;
 }
 
-double Power(double param1, int param2)
+double Power(double leftOperand, int rightOperand)
 {
   double result = 1;
-  if (param2 < 0)
+  if (rightOperand < 0)
     throw "Power cannot be negative.";
-  else if (param1 == 0 && param2 <= 0)
+  else if (leftOperand == 0 && rightOperand <= 0)
     throw "0 power of 0 is not defined.";
 
-  if (param2 == 0)
+  if (rightOperand == 0)
     return result = 1;
 
-  for (int i = 1; i <= param2; i++)
+  for (int i = 1; i <= rightOperand; i++)
   {
-    result *= param1;
+    result *= leftOperand;
   }
   return result;
 }
--- src/math_lib/mathematical_library.h
+++ src/math_lib/mathematical_library.h
@@ -9,27 +9,42 @@
 //  mathematical_library.h created: 2018-Mar-09
 //===========================================================
 
-double Add(double param1, double param2);
+double Add(double leftOperand, double rightOperand);
 
 /**
  * Subtract two numbers
- * @param param1 - minuend
- * @param param2 - subtrahend
+ * @param leftOperand - minuend
+ * @param rightOperand - subtrahend
  * @return result
  */
-double Subtract(double param1, double param2);
+double Subtract(double leftOperand, double rightOperand);
 
-double Multiply(double param1, double param2);
-double Divide(double param1, double param2);
+/**
+ * Multiply two numbers
+ * @param leftOperand - multiplicand
+ * @param rightOperand - multiplier
+ * @return result
+ */
+double Multiply(double leftOperand, double rightOperand);
+
+
+double Divide(double leftOperand, double rightOperand);
 
 /**
  * Natural power of number
- * @param param1 - number
- * @param param2 - power
+ * @param leftOperand - number
+ * @param rightOperand - power
  * @return result
  */
-double Power(double param1, int param2);
+double Power(double leftOperand, int rightOperand);
 
-double SquareRoot(double param);
-int Factorial(int param);
-double Modulo(double param1, double param2);
+double SquareRoot(double operand);
+int Factorial(int operand);
+
+/**
+ * Remainder of the division of leftOperand by rightOperand
+ * @param leftOperand - dividend
+ * @param rightOperand - divisor
+ * @return result
+ */
+double Modulo(int leftOperand, int rightOperand);
