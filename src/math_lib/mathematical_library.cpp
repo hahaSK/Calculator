b876cb4a0d3f589a9817cbcc184d6e951781cbc4
hahaSK <xlahvi00@vutbr.cz>
2018-03-19 18:55:13 +0100

    Bugfixes and clean code improvments
    Ready for merge to master
    
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

--- src/math_lib/mathematical_library.cpp
+++ src/math_lib/mathematical_library.cpp
@@ -13,7 +13,7 @@
 
 double Add(double leftOperand, double rightOperand)
 {
-	return leftOperand + rightOperand;
+  return leftOperand + rightOperand;
 }
 
 double Subtract(double leftOperand, double rightOperand)
@@ -30,12 +30,18 @@ double Power(double leftOperand, int rightOperand)
 {
   double result = 1;
   if (rightOperand < 0)
+  {
     throw "Power cannot be negative.";
+  }
   else if (leftOperand == 0 && rightOperand <= 0)
+  {
     throw "0 power of 0 is not defined.";
+  }
 
   if (rightOperand == 0)
+  {
     return result = 1;
+  }
 
   for (int i = 1; i <= rightOperand; i++)
   {
@@ -46,78 +52,78 @@ double Power(double leftOperand, int rightOperand)
 
 int Factorial(int operand)
 {
-	int result = operand;
+  int result = operand;
 
-	if (operand == 1 && operand == 0)
-	{
-		return 1;
-	}
+  if (operand == 1 || operand == 0)
+  {
+    return 1;
+  }
 
-	if (operand < 0)
-	{
-		throw "Number for factorial cannot be negative";
-	}
+  if (operand < 0)
+  {
+    throw "Number for factorial cannot be negative";
+  }
 
-	for (int i = operand - 1; i > 0; i--)
-	{
-		result *= i;
-	}
+  for (int i = operand - 1; i > 0; i--)
+  {
+    result *= i;
+  }
 
-	return result;
+  return result;
 }
 
-//int Modulo(int leftOperand, int rightOperand)
-//{
-//  int result;
-//  if (rightOperand == 0)
-//  {
-//    throw "Division by zero is not defined.";
-//  }
-//
-//  result = int(Divide(leftOperand, rightOperand));
-//  return int(Subtract(leftOperand,Multiply(rightOperand,result)));
-//}
+int Modulo(int leftOperand, int rightOperand)
+{
+  int result;
+  if (rightOperand == 0)
+  {
+    throw "Division by zero is not defined.";
+  }
+
+  result = int(Divide(leftOperand, rightOperand));
+  return int(Subtract(leftOperand, Multiply(rightOperand, result)));
+}
 
 double Divide(double leftOperand, double rightOperand)
 {
-	if (rightOperand == 0)
-	{
-		throw "Cannot divide with 0.";
-	}
+  if (rightOperand == 0)
+  {
+    throw "Cannot divide with 0.";
+  }
 
-	else
-		return (leftOperand / rightOperand);
+  else
+    return (leftOperand / rightOperand);
 }
 
 double SquareRoot(double operand)
 {
-	if (operand < 0)
-	{
-		throw "Cannot make square root from number lower than 0.";
-	}
+  if (operand < 0)
+  {
+    throw "Cannot make square root from number lower than 0.";
+  }
 
-	double low = 0;
-	double mid = 0;
-	double high = operand;
+  double low = 0;
+  double mid = 0;
+  double high = operand;
 
-	for (unsigned i = 0; i < 1000; i++)
-	{
-		mid = (low + high) / 2;
+  for (unsigned i = 0; i < 1000; i++)
+  {
+    mid = (low + high) / 2;
 
-		if (mid * mid == operand)
-		{
-			return mid;
-		}
+    if (mid * mid == operand)
+    {
+      return mid;
+    }
 
-		else if (mid * mid > operand)
-		{
-			high = mid;
-		}
+    else if (mid * mid > operand)
+    {
+      high = mid;
+    }
 
-		else
-		{
-			low = mid;
-		}
-	}
-	return mid;
+    else
+    {
+      low = mid;
+    }
+  }
+  return mid;
 }
--- src/tests/mathematical_library_tests.cpp
+++ src/tests/mathematical_library_tests.cpp
@@ -14,51 +14,45 @@
 #include "googletest-master/googlemock/include/gmock/gmock.h"
 #include "../math_lib/mathematical_library.h"
 
-TEST(testing, sub)
+TEST(Addition, Basics)
 {
-  EXPECT_EQ(Subtract(5,5),0);
-}
-
-
-TEST(testing, Add)
-{
- EXPECT_EQ(Add(5,5),10);
+ ASSERT_EQ(Add(5,5),10);
  EXPECT_EQ(Add(10,-5),5);
  EXPECT_EQ(Add(-15,-10),-25);
  EXPECT_EQ(Add(3.3, 1.2),4.5);
 }
 
-TEST(testing, Factorial)
+TEST(Factorial, Basics)
 {
   EXPECT_EQ(Factorial(0),1);
-  EXPECT_EQ(Factorial(5),120);
-  EXPECT_EQ(Factorial(-5),-120);
+  ASSERT_EQ(Factorial(5),120);
+  ASSERT_ANY_THROW(Factorial(-5));
 }
 
-//TEST(testing, Dividing)
-//{
-//  EXPECT_EQ(Divide(0, 42), 0);
-//  ASSERT_EQ(Divide(196, 14), 14);
-//  ASSERT_ANY_THROW(Divide(142, 0));
-//}
-//
-//TEST(testing, Modulo)
-//{
-//  ASSERT_EQ(Modulo(0, 42), 0);
-//  ASSERT_EQ(Modulo(196, 42), 28);
-//  ASSERT_ANY_THROW(Modulo(142, 0));
-//}
-//
-//TEST(testing, SquareRoot)
-//{
-//  ASSERT_EQ(SquareRoot(0), 0);
-//  ASSERT_EQ(SquareRoot(196), 14);
-//  ASSERT_ANY_THROW(SquareRoot(-42));
-//}
-
-TEST(testing, Multiply)
+TEST(Dividing, Basics)
 {
-  EXPECT_EQ(Multiply(2,2),4);
+  EXPECT_EQ(Divide(0, 42), 0);
+  ASSERT_EQ(Divide(196, 14), 14);
+  ASSERT_ANY_THROW(Divide(142, 0));
+}
+
+TEST(Modulo, Basics)
+{
+  ASSERT_EQ(Modulo(0, 42), 0);
+  ASSERT_EQ(Modulo(196, 42), 28);
+  ASSERT_ANY_THROW(Modulo(142, 0));
+}
+
+TEST(SquareRoot, Basics)
+{
+  ASSERT_EQ(SquareRoot(0), 0);
+  ASSERT_EQ(SquareRoot(196), 14);
+  ASSERT_ANY_THROW(SquareRoot(-42));
+}
+
+TEST(Multiply, Basics)
+{
+  ASSERT_EQ(Multiply(2,2),4);
   EXPECT_EQ(Multiply(2,-2),-4);
   EXPECT_EQ(Multiply(0,4),0);
   EXPECT_EQ(Multiply(1,10),10);
@@ -66,24 +60,22 @@ TEST(testing, Multiply)
 
 TEST(Substraction, Basics)
 {
-	ASSERT_EQ(sub(2, 2) == 0)
-	ASSERT_EQ(sub(2, 7) == -5);
-	ASSERT_EQ(sub(25, 100) == -75);
-	ASSERT_EQ(sub(100, 25) == 75);
+	ASSERT_EQ(Subtract(2, 2), 0);
+	ASSERT_EQ(Subtract(2, 7), -5);
+	ASSERT_EQ(Subtract(25, 100), -75);
+	ASSERT_EQ(Subtract(100, 25), 75);
 }
 
 TEST(Power, Basics)
 {
-	ASSERT_EQ(pow(2, 2) == 4);
-	ASSERT_EQ(pow(2, 10) == 1024);
-	ASSERT_EQ(pow(-5, 2) == 25);
-	ASSERT_EQ(pow(-7, 8) == 5764801);
-	ASSERT_ANY_THROW(pow(2, -5));
-	ASSERT_ANY_THROW(pow(4, 3.2));
-	ASSERT_ANY_THROW(2, -4.5);
-	ASSERT_EQ(pow(7, 0) == 1);
-	ASSERT_EQ(pow(22, 3) == 10648);
-	ASSERT_EQ(pow(-15, 5) == -759375);
+	ASSERT_EQ(Power(2, 2), 4);
+	ASSERT_EQ(Power(2, 10), 1024);
+	ASSERT_EQ(Power(-5, 2), 25);
+	ASSERT_EQ(Power(-7, 8), 5764801);
+	ASSERT_ANY_THROW(Power(2, -5));
+	ASSERT_EQ(Power(7, 0), 1);
+	ASSERT_EQ(Power(22, 3), 10648);
+	ASSERT_EQ(Power(-15, 5), -759375);
 }
 
 int main(int argc, char *argv[])
