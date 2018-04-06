7efeebfd7ef8f7c5a6cb123f31128e16f80f0e99
hahaSK <xlahvi00@vutbr.cz>
2018-04-07 00:18:17 +0200

    GUI implementation
    GUI implemented through CMake.
    LINUX - TESTED, OK
    WINDOWS - NOT TESTED
    
    Signed-off-by: hahaSK <xlahvi00@vutbr.cz>

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,7 @@
-//=====================ShatteredButWhole=====================
-//              IVS project 2 - Calculator
-//
-//  Authors: Juraj Lahvička <xlahvi00@vutbr.cz>
-//           Lubomír Švehla <xsvehl09@stud.fit.vutbr.cz>
-//           Tomáš Sasák    <xsasak01@stud.fit.vutbr.cz>
-//           Martin Krajčí  <xkrajc21@stud.fit.vutbr.cz>
-//
-//  main.cpp created: 2018-Mar-09
-//===========================================================
-
-
-#include <iostream>
-#include <typeinfo>
-#include "math_lib/mathematical_library.h"
-
-#include "GUI/Calculator/mainwindow.h"
+#include "mainwindow.h"
 #include <QApplication>
 
-int main()
+int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
